3ff0071d8b68007bf9a9fd8ae9af39d0fb20b4ae
Gilberto de Freitas Lins <dilbert.gil@gmail.com>
2021-04-13 16:21:07 -0300

    mensagem de bom dia

--- src/application/program.java
+++ src/application/program.java
@@ -3,8 +3,7 @@ package application;
 public class program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("commit 2");
+		System.out.println("Bom dia");
 	}
 
 }
