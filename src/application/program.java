1bef2e4da6c95489a27556223f7f0f31706ffcfc
Gilberto de Freitas Lins <dilbert.gil@gmail.com>
2021-04-13 17:05:49 -0300

    acrescentei um for

--- src/application/program.java
+++ src/application/program.java
@@ -5,6 +5,11 @@ public class program {
 	public static void main(String[] args) {
 		System.out.println("Bom dia");
 		System.out.println("com vai?");
+		System.out.println("com vai?");
+		System.out.println("com vai?");
+		for ( int i =0; i <  10; i++)
+			System.out.println(i);
+		
 	}
 
 }
